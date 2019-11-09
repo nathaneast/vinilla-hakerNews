09efcc97974eb44d7f7160087e6f8d4f7b9429af
nathaneast <ahrl_ahrl@naver.com>
2019-11-10 01:42:30 +0900

    css완성

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ $.ajax({
         for (var i = 0; 10 > i; i++) {
             $.get(`https://hacker-news.firebaseio.com/v0/item/${items[i]}.json?print=pretty`, function (item) {
                 var li = document.createElement("li");
+                var postsNum = document.createElement("div");
+                var postsText = document.createElement("div");
                 var firstLine = document.createElement("div");
                 var secondLine = document.createElement("div");
                 var num = document.createElement("span");
@@ -17,34 +19,42 @@ $.ajax({
                 var by = document.createElement("span");
                 var time = document.createElement("span");
                 var coment = document.createElement("span");
+                var hide = document.createElement("span");
+                var space = "\u00A0";
 
-                firstLine.classList.add("content-line");
-                secondLine.classList.add("content-line");
-                title.classList.add("content-title");
-                num.classList.add("content-num");
-                url.classList.add("content-others");
-                score.classList.add("content-others");
-                by.classList.add("content-others");
-                time.classList.add("content-others");
-                coment.classList.add("content-others");
+                li.classList.add("posts");
+                postsNum.classList.add("postsNum");
+                num.classList.add("postsNum-num");
+                title.classList.add("postsText-title");
+                url.classList.add("postsText-others");
+                score.classList.add("postsText-others");
+                by.classList.add("postsText-others");
+                time.classList.add("postsText-others");
+                hide.classList.add("postsText-others");
+                coment.classList.add("postsText-others");
 
-                num.innerText = n+". ";
-                title.innerText = item.title+" ";
-                url.innerText = item.url;
+                num.innerText = n + "." + space;
                 contents.appendChild(li);
-                li.appendChild(firstLine);
-                li.appendChild(secondLine);
-                firstLine.appendChild(num);
+                li.appendChild(postsNum);
+                li.appendChild(postsText);
+                postsNum.appendChild(num);
+
+                title.innerText = item.title + space;
+                url.innerText = item.url;
+                postsText.appendChild(firstLine);
+                postsText.appendChild(secondLine);
                 firstLine.appendChild(title);
                 firstLine.appendChild(url);
 
-                score.innerText = item.score+" points ";
+                score.innerText = item.score + " points ";
                 by.innerText = "by "+item.by;
-                time.innerText = " "+item.time
-                coment.innerText = " "+item.descendants+" coments"
+                time.innerText = space + item.time;
+                hide.innerText = " | hide |";
+                coment.innerText = space + item.descendants + " coments";
                 secondLine.appendChild(score);
                 secondLine.appendChild(by);
                 secondLine.appendChild(time);
+                secondLine.appendChild(hide);
                 secondLine.appendChild(coment);
                 n++;
             });
